b2797615d934c09dda80460c34bbecc47145da7a
sahar <saharhallaji.dev@gmail.com>
2023-09-28 19:09:21 +0330

    add login api

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import User from './models/User'
 import express, {Express, Request, Response} from "express";
 
@@ -11,7 +12,7 @@ mongoose.connect('mongodb://localhost:27017/auth_expressjs')
 
 app.post('/account/register', async (req: Request, res: Response) => {
     const {email, password} = req.query;
-    if (!email || !password || email == "" || password == "") return res.status(400).json({message: 'please enter email and password'})
+    if (!(email || password) || email == "" || password == "") return res.status(400).json({message: 'please enter email and password'})
 
     const userExisting = await User.findOne({email: email})
     if (userExisting) return res.status(400).json({message: `${email} already exists`})
@@ -28,14 +29,20 @@ app.post('/account/register', async (req: Request, res: Response) => {
 
 app.post('/account/extraInfo', async (req: Request, res: Response) => {
     const {firstName, lastName, phoneNumber, email} = req.query;
-    if (!firstName || !lastName || phoneNumber || !email || email == "") return res.status(400).json({message: 'please enter phone number and first name and last name'})
+    if (!(firstName || lastName || phoneNumber || email) || email == "") return res.status(400).json({message: 'please enter phone number and first name and last name'})
     await User.findOneAndUpdate({email: email}, {phoneNumber: phoneNumber, firstName: firstName, lastName: lastName})
     return res.status(200).json({message: "user updated successfully!"})
 })
 
 
-app.post('/account/login', (req: Request, res: Response) => {
-
+app.post('/account/login', async(req: Request, res: Response) => {
+    const {email , password} = req.query
+    if (!(email || password)) return res.status(400).json({ message: 'enter the correct username and password' })
+    const user = await User.findOne({email: email })
+    if (!user) return res.status(400).json({ message: 'user does not exist' })
+    const passwordMatch = await bcrypt.compare(password as string, user.password)
+    if (!passwordMatch) return res.status(401).json({message : "password is incorrect!"})
+    const token = jwt.sign()
 })
 
 app.listen(5000)
