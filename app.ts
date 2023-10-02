b7184af1c6e9fbc567f13adb8954ccf09b52a203
sahar <saharhallaji.dev@gmail.com>
2023-10-02 22:05:05 +0330

    add swagger to project

--- app.ts
+++ app.ts
@@ -6,13 +6,37 @@ import express, {Express, Request, Response} from "express";
 import checkToken from "./middleware/checkToken";
 import AuthRequest from "./interfaces";
 import crypto from "crypto"
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUiExpress from "swagger-ui-express"
 
-
-export const secretKey = Object.freeze(crypto.randomBytes(64).toString("base64"))
+const devMode = true
+export const secretKey = devMode === true ? "ddibidbidbid3093u03" : Object.freeze(crypto.randomBytes(32).toString('hex'))
 
 
 const app: Express = express()
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: "authentication project using express.js apis",
+            version: "0.1.0",
+            description: "a sample project for my github account!",
+            contact: {
+                name: "Sahar Hallaji",
+                email: "saharhallaji.dev@gmail.com",
+            }
+        },
+        servers: [
+            {
+                url: "http://localhost:5000/"
+            }
+        ]
+    },
+    apis: ["./routes/*.ts"]
+}
+const spaces = swaggerJSDoc(options as object)
+app.use("/api-docs", swaggerUiExpress.serve, swaggerUiExpress.setup(spaces, {explorer: true}))
 mongoose.connect('mongodb://localhost:27017/auth_expressjs')
     .then(() => console.log('connected to MongoDB'))
     .catch(err => console.error(err))
@@ -22,23 +46,44 @@ app.post('/account/register', async (req: Request, res: Response) => {
     const {email, password} = req.query;
     if (!(email || password) || email == "" || password == "") return res.status(400).json({message: 'please enter email and password'})
 
-    const userExisting = await User.findOne({email: email})
-    if (userExisting) return res.status(400).json({message: `${email} already exists`})
+    await User.findOne({email: email})
+        .then((result) => {
+            if (result) return res.status(409).json({message: `${email} already exists`})
+        })
+        .catch((err) => {
+            console.log(err)
+            return res.status(500).json({message: `something went wrong`})
+        })
 
-    const hashedPass = await bcrypt.hash(password as string, 10)
+    let hashedPass = null
+    await bcrypt.hash(password as string, 10)
+        .then((result) => {
+            hashedPass = result
+        })
+        .catch((err) => {
+            console.log(err)
+            return res.status(500).json({message: `something went wrong`})
+        })
+
     const user = new User({email: email, password: hashedPass, firstName: null, lastName: null, phoneNumber: null})
-
     await user.save()
-
-    return res.status(201).send('user registered successfully.')
-
-
+        .then(() => {
+            return res.status(200).json({message: 'user registered successfully.'})
+        })
+        .catch((err) => {
+            console.log(err)
+            return res.status(400).json({message: 'request failed'})
+        })
 })
 
 app.post('/account/extraInfo', checkToken, async (req: AuthRequest, res: Response) => {
     const {firstName, lastName, phoneNumber} = req.query;
     if (!(firstName || lastName || phoneNumber)) return res.status(400).json({message: 'please enter phone number and first name and last name'})
-    await User.findOneAndUpdate({email: req.user.email}, {phoneNumber: phoneNumber, firstName: firstName, lastName: lastName}, {upsert:true})
+    await User.findOneAndUpdate({email: req.user.email}, {
+        phoneNumber: phoneNumber,
+        firstName: firstName,
+        lastName: lastName
+    }, {upsert: true})
         .then((data) => {
             return res.status(200).json({
                 message: "user updated successfully!",
@@ -58,15 +103,17 @@ app.post('/account/extraInfo', checkToken, async (req: AuthRequest, res: Respons
 app.post('/account/login', async (req: Request, res: Response) => {
     const {email, password} = req.query
     if (!(email || password)) return res.status(400).json({message: 'enter the correct username and password'})
-    try {
-        const user = await User.findOne({email: email})
-        if (!user || !(await bcrypt.compare(password as string, user.password as string))) return res.status(400).json({message: 'email or password is incorrect!'})
-        const token = jwt.sign({email: user.email}, secretKey, {expiresIn: '1h'})
-        return res.status(200).json({message: "login successful!", token})
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({message: "something went wrong!"})
-    }
+    await User.findOne({email: email})
+        .then(async (user) => {
+            if (!user || !(await bcrypt.compare(password as string, user.password as string))) return res.status(400).json({message: 'email or password is incorrect!'})
+            const token = jwt.sign({email: user.email}, secretKey, {expiresIn: '1h'})
+            return res.status(200).json({message: "login successful!", token})
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({message: "something went wrong!"})
+        })
+
 })
 
 app.get('/user/panel', checkToken, (req: AuthRequest, res: Response) => {
